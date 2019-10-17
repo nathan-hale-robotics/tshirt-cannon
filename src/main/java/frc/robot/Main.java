772343a90ed64c833cc01b10c7b2f1eb72530575
Nathan Hale Robotics Laptop <nhalerobotics@gmail.com>
2019-10-17 16:28:06 -0700

    First commit

---
+++ src/main/java/frc/robot/Main.java
@@ -0,0 +1,10 @@
+
+package frc.robot;
+
+import edu.wpi.first.wpilibj.RobotBase;
+
+public final class Main {
+  public static void main(String... args) {
+    RobotBase.startRobot(Robot::new);
+  }
+}
