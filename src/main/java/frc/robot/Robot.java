fb547bb76da0740e2f6a47424bba01244b21e4bc
Dylan Gyori <gyoridylan@gmail.com>
2019-10-18 18:42:36 -0700

    added documentation
    documented functions under Robot class and added TODO's where info about the code may be needed. Some of the documentation on function arguments may not be needed if functions of used objects already contain documentation on their parameters.

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -6,17 +6,49 @@ import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj.drive.DifferentialDrive;
 
 public class Robot extends TimedRobot {
+	/*drive represents a tank
+	 *drivetrain. */
   private DifferentialDrive drive;
+ 	//TODO how is the x,y,z coordinate plane mapped to the joystick?
   private Joystick driveStick;
 
+  /**
+   * robotInit (robot initialization)
+   * is called when robot is turned
+   * on.
+   */
   @Override
   public void robotInit() {
-    drive = new DifferentialDrive(new Spark(0), new Spark(1));
-    driveStick = new Joystick(0);
+  	/*TODO I am assuming the spark 
+  	 * object lets you interface 
+  	 * with a pwm signal that
+  	 * indirectly controls the
+  	 * motor connected to it.
+  	 */
+    drive = 
+    	new DifferentialDrive(
+    		//left motor
+    		new Spark(0),
+    		//right motor 
+    		new Spark(1));
+    //TODO is port number usb port number?
+    driveStick = 
+    new Joystick(0);
   }
 
+	/**
+	 * teleop is called
+	 * periodically (every m_period
+	 * seconds) when the robot is
+	 * in teleop mode.
+	 */
   @Override
   public void teleopPeriodic() {
-    drive.arcadeDrive(driveStick.getX(), driveStick.getY());
+  	//set drivetrain's speeds
+    drive.arcadeDrive(
+    	//forward speed
+    	driveStick.getX(), 
+    	//clockwise rotation speed
+    	driveStick.getY());
   }
 }
