f5ca57ddfedb76950ab83f8a226daa1806b8ca25
Dylan Gyori <gyoridylan@gmail.com>
2019-10-23 16:57:20 -0700

    Revert "Made Robot.java contain code for TShirt cannon"
    This reverts commit c67d59ee060aa209547cf5639b80db756097c3d4.

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -1,44 +1,61 @@
 package frc.robot;
 
-import edu.wpi.first.wpilibj.DigitalOutput;
 import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.Spark;
 import edu.wpi.first.wpilibj.TimedRobot;
+import edu.wpi.first.wpilibj.drive.DifferentialDrive;
 
 public class Robot extends TimedRobot {
-  private Joystick joystick;
-  private Spark compressor;
-  private boolean runningCompressor = false;
-  private DigitalOutput valve;
+	/*drive represents a tank
+	 *drivetrain. */
+  private DifferentialDrive drive;
+  /*interface for speed of right
+   *motor.*/
+  private Spark rMotor;
+  /*interface for speed of left
+   *motor.*/
+  private Spark lMotor;
+  //TODO how is the x,y,z coordinate plane mapped to the joystick?
+  private Joystick driveStick;
 
+  /**
+   * robotInit (robot initialization)
+   * is called when robot is turned
+   * on.
+   */
   @Override
   public void robotInit() {
-    // loads the first joystick attached
-    joystick = new Joystick(0);
-    // sets the pin 0 to be a Spark (motor)
-    compressor = new Spark(0);
-    // sets the pin 1 to be a DigitalOutput (toggle output)
-    valve = new DigitalOutput(1);
+    //Assigning motors to roborio
+    lMotor = new Spark(0);
+    rMotor = new Spark(1);
+  	/*TODO I am assuming the spark 
+  	 * object lets you interface 
+  	 * with a pwm signal that
+  	 * indirectly controls the
+  	 * motor connected to it.
+  	 */
+    drive = 
+    	new DifferentialDrive(
+    		lMotor,
+    		rMotor);
+    //TODO is port number usb port number?
+    driveStick = 
+    new Joystick(0);
   }
 
+	/**
+	 * teleop is called
+	 * periodically (every m_period
+	 * seconds) when the robot is
+	 * in teleop mode.
+	 */
   @Override
   public void teleopPeriodic() {
-    // will run the compressor
-    // can set the value to 0.5, if want to run at half speed
-    if (runningCompressor) {
-      compressor.set(1);
-    } else {
-      compressor.set(0);
-    }
-    // will toggle the compressor if the user presses a button
-    if (joystick.getRawButtonPressed(1)) {
-      runningCompressor = !runningCompressor;
-    }
-    // will open the valve as long as the trigger is pressed
-    if (joystick.getRawButton(0)) {
-      valve.set(true);
-    } else {
-      valve.set(false);
-    }
+  	//set drivetrain's speeds
+    drive.arcadeDrive(
+    	//forward speed
+    	driveStick.getX(), 
+    	//clockwise rotation speed
+    	driveStick.getY());
   }
 }
