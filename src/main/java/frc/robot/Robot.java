9820a7bdf7b5be22945c1d13c2e7c10aa44ea34c
Nathan Hale Robotics Laptop <nhalerobotics@gmail.com>
2019-10-25 09:03:04 -0700

    Made valve close after half a second

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -4,29 +4,32 @@ import edu.wpi.first.wpilibj.DigitalOutput;
 import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.Spark;
 import edu.wpi.first.wpilibj.TimedRobot;
+import edu.wpi.first.wpilibj.Timer;
 
 public class Robot extends TimedRobot {
   private Joystick joystick;
   private Spark compressor;
-  private boolean runningCompressor = false;
   private DigitalOutput valve;
+  private Timer timer;
 
   @Override
   public void robotInit() {
     joystick = new Joystick(0);
     compressor = new Spark(0);
     valve = new DigitalOutput(1);
+		timer = new Timer();
   }
 
   @Override
   public void teleopPeriodic() {
-    if (runningCompressor) {
+    if (timer.get() < 0.5) {
       compressor.set(1);
     } else {
       compressor.set(0);
     }
     if (joystick.getRawButtonPressed(1)) {
-      runningCompressor = !runningCompressor;
+      timer.reset();
+      timer.start();
     }
     if (joystick.getRawButton(0)) {
       valve.set(true);
