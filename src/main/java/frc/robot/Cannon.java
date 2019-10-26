76f8370785b31ca21859c510c44d41f451eb2d56
Dylan Gyori <gyoridylan@gmail.com>
2019-10-26 16:03:07 -0700

    Finished Cannon class for now

--- src/main/java/frc/robot/Cannon.java
+++ src/main/java/frc/robot/Cannon.java
@@ -4,24 +4,68 @@ import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj.DigitalOutput;
 import edu.wpi.first.wpilibj.Spark;
 
+//TODO at what point is the cannon considered disarmed? at what point is the airTank considered full?
 /*A Cannon consists of any valves,
  * pressurized chambers, and 
  * compressors.*/
 public class Cannon {
-	/*Used as a stop watch to
-   * keep track of compressor
-   * on duration (compressor time)*/
-  private Timer compTime;
-	//general timer/stop-watch
-  //private Timer timer;
+	//Default valve open duration
+	//when cannon is fired.
+	private static final double
+		DEFAULT_FIRE = 0.5;
+
+	//how long air tank's
+	//compressor ran in total
+	//since tankTime reset.
+	//TODO not fully implemented
+	private double tankTime =
+		0.0;
+
+	//Used to pressurize
+	//cannon's air tank.
   private Spark compressor;
-  //is compressor running?
-  private boolean runningCompressor = 
-  	false;
-  /*valve controlling whether
-   * pressurized chamber is open
-   * to cannon barrel.*/
+  //How long the compressor is
+  //set to run for since its
+  //timer has been reset.
+  //(compressor Time)
+  private double compTime = 
+  	0.0;
+  //Used as a stop watch to
+  //keep track of compressor
+  //on duration.
+  //(compressor timer)
+  private Timer compTimer;
+  
+  //valve controlling whether
+  //pressurized chamber is open
+  //to cannon barrel.
   private DigitalOutput valve;
+  //How long the valve is
+  //set to be open for since 
+  //its timer has been reset.
+  private double valveTime = 
+  	0.0;
+  //Used as a stop watch to
+  //keep track of valve
+  //open duration.
+  private Timer valveTimer;
+  
+  //Is cannon ready to fire?
+  private boolean isArmed =
+		false;
+	//Does air tank contain
+	//pressure?
+	private boolean hasPressure =
+		false;
+	//Is compressor finished
+	//pressurizing the air tank?
+	private boolean isFinished =
+		false;
+	//number of times cannon
+	//was fired since it was
+	//considered armed
+	private int numFired =
+		0;
   
   /**assign Cannon to pins*/
 	public Cannon(
@@ -32,8 +76,110 @@ public class Cannon {
 		this.compressor = 
 			compressor;
 		this.valve = valve;
-		//set timer to 0
-		this.compTime.reset();
+		
+		this.compTimer.reset();
+		this.valveTimer.reset();
 	}
-
+	/**
+	 *Updates state of cannon
+	 *components based on time
+	 *they've ran.
+	 */
+	public void update() {
+		//update compressor state
+		if(compTime >
+			compTimer.get())
+			compressor.set(1);
+		else
+			compressor.set(0);
+		
+		//update valve state
+		//TODO is false closed and true open?
+		if(valveTime >
+			valveTimer.get())
+			valve.set(true);
+		else
+			valve.set(false);
+			
+		//update isFinished state
+		if(compTimer.get() >
+			compTime &&
+			!isFinished)
+			isFinished = true;
+		
+		//update isArmed state
+		if(isFinished &&
+			hasPressure &&
+			!isArmed) {
+			isArmed = true;
+			numFired = 0;
+		}
+	}
+	
+	/**
+	 * turns on air tank's
+	 * compressor for a set 
+	 * amount of time.
+	 */
+	public void arm(
+		//Time compressor will run
+		double compTime) {
+		this.compTime =
+			compTime;
+		compTimer.reset();
+		hasPressure = true;
+		isFinished = false;
+	}
+	
+	/**
+	 * sets cannon to a
+	 * disArmed state
+	 */
+	public void disArm() {
+		fire(2.0);
+		isArmed = false;
+		hasPressure = false;
+	}
+	
+	/**
+	 * open air tank to cannon
+	 * barrel for given amount
+	 * of time.
+	 * (fire the cannon)
+	 */
+	public void fire(
+		//Time valve is opened for
+		double valveTime) {
+		this.valveTime =
+			valveTime;
+		valveTimer.reset();
+		++numFired;
+	}
+	
+	public void fire() {
+		this.fire(DEFAULT_FIRE);
+	}
+	
+	/**
+	 * Is cannon ready to fire?
+	 */
+	public boolean isArmed() {
+		return isArmed;
+	}
+	
+	/**
+	 * Is compressor finished
+	 * running?
+	 */
+	public boolean isFinished() {
+		return isFinished;
+	}
+	
+	/**
+	 * Does air tank potentially
+	 * contain air pressure?
+	 */
+	public boolean hasPressure() {
+		return hasPressure;
+	}
 }
