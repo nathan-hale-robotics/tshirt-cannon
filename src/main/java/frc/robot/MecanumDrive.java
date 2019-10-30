d51c7ac3fe74dc1517021326b5600dfed56d2a68
Dylan Gyori <gyoridylan@gmail.com>
2019-10-30 09:05:25 -0700

    Finished speed function
    speed allows user to ajust various robot velocities.

--- src/main/java/frc/robot/MecanumDrive.java
+++ src/main/java/frc/robot/MecanumDrive.java
@@ -2,6 +2,10 @@ package frc.robot;
 
 import edu.wpi.first.wpilibj.Spark;
 
+/**
+ * A class used to interface with a mecanum
+ * wheel drivetrain.
+ */
 public class MecanumDrive {
 
   private Spark frontLeft;
@@ -9,17 +13,35 @@ public class MecanumDrive {
   private Spark backLeft;
   private Spark backRight;
 
-  public MecanumDrive(int frontLeft, int frontRight, int backLeft, int backRight) {
+  public MecanumDrive(int frontLeft, 
+    int frontRight, int backLeft, int backRight) {
     this.frontLeft = new Spark(frontLeft);
     this.frontRight = new Spark(frontRight);
     this.backLeft = new Spark(backLeft);
     this.backRight = new Spark(backRight);
   }
 
-  public void move(double speed, double strafe, double turn) {
-    frontLeft.set(speed - strafe - turn);
-    frontRight.set(speed + strafe + turn);
-    backLeft.set(speed + strafe - turn);
-    backRight.set(speed - strafe + turn);
+  /**
+   * Ajust rotation velocity and directional 
+   * velocity. Expected input range: -1 to 1.
+   * @param forward forward velocity
+   * @param strafe rightward velocity
+   * @param turn clockwise rotation velocity
+   */
+  public void speed(double forward, double strafe, 
+    double turn) {
+    //This ensures the absolute value sum of all
+    //velocities is 1 or under.
+    double forwardR = forward/(1 +
+      abs(strafe) + abs(turn));
+    double strafeR = strafe/(1 +
+      abs(forward) + abs(turn));
+    double turnR = turn/(1 +
+      abs(strafe) + abs(forward));
+    
+    frontLeft.set(forwardR + strafeR + turnR);
+    frontRight.set(forwardR - strafeR - turnR);
+    backLeft.set(forwardR - strafeR + turnR);
+    backRight.set(forwardR + strafeR - turnR);
   }
 }
